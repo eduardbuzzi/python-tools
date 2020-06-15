932aabd44846c40667e83c632dd297a05f1461a8
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-06-14 23:44:08 -0300

    Create clienttcp-file.py

---
+++ clienttcp-file.py
@@ -0,0 +1,16 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(1)
+
+ip = str(raw_input("Informe o IP para se conectar => "))
+port = int(raw_input("Informe a Porta para se conectar => "))
+
+try:
+    client.connect((ip, port))
+    data = raw_input("Enviar => ")
+    client.send(data)
+    pacotes = client.recv(1024)
+    print pacotes
+except:
+    print "Conexao falhou!"
