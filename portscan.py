c6f8a85df4dbedc9581e3b541fe86fb96330cd1d
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-06-20 23:44:42 -0300

    Create portscan.py

---
+++ portscan.py
@@ -0,0 +1,15 @@
+# Python 2.7
+
+import socket
+import os
+ip = raw_input("Informe o IP => ")
+ports = range(65535)
+
+for port in ports:
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.6)
+    code = client.connect_ex((ip, port))
+
+    if code == 0:
+        print str(port) + " => Porta Aberta"
