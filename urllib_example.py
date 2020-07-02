b94a92f3809159cd9622436d9dbf86ad146642d5
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-07-02 14:50:57 -0300

    Create urllib_example.py

---
+++ urllib_example.py
@@ -0,0 +1,16 @@
+import urllib2
+import urllib
+
+url = 'https://solyd.com.br/'
+cabecalho = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Mobile Safari/537.36',
+}
+
+try:
+
+    requisicao = urllib2.Request(url, headers=cabecalho)
+    resposta = urllib2.urlopen(requisicao)
+    html = resposta.read()
+    print html
+except Exception as erro:
+    print erro
